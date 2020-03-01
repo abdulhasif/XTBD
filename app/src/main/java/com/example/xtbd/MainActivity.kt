e00e1ba2248c4fd58c4920d57e9b085bc0db255e
abdulhasif <abdulhasif99@gmail.com>
2020-03-01 21:20:16 +0530

    login page

--- app/src/main/java/com/example/xtbd/MainActivity.kt
+++ app/src/main/java/com/example/xtbd/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.xtbd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed( {
+            startActivity(Intent(this@MainActivity,Login::class.java ))
+            finish()
+        },3000)
     }
 }
